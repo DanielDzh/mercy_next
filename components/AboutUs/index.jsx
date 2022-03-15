e06c765a66f47590af02176cf118ac38b6dd99e6
Your Name <you@example.com>
2022-03-15 20:34:01 +0200

    mobile

--- components/AboutUs/index.jsx
+++ components/AboutUs/index.jsx
@@ -4,41 +4,48 @@ import CustomList from "../CustomList";
 import styles from "./AboutUs.module.scss";
 import ButtonBlue from "../ButtonBlue";
 import { useTrans } from "../../hooks/useTrans";
+import { useOpenHandlers } from "../../hooks/useOpenHandlers";
+import PaymentForm from "../PaymentForm";
 
 export default function AboutUs() {
+
+  const { open, onOpen, onClose } = useOpenHandlers(false);
 
   const { trans } = useTrans();
 
   return (
-    <div id="aboutUs">
-      <div className={styles["aboutUs-container"]}>
-        <div className={styles["aboutUs-img"]}>
-          <img src="images/we.png" alt="" loading="lazy" className={styles["aboutUs-img-img"]} />
-          <div className={styles["aboutUs_desc_tablet"]}>
-            <SectionTitle
-              title={trans("aboutUs_title")}
-              desc={trans("aboutUs_desc_under_title")}
-            />
-            <CustomList />
+    <>
+      <PaymentForm open={open} onClose={onClose} />
+      <div id="aboutUs">
+        <div className={styles["aboutUs-container"]}>
+          <div className={styles["aboutUs-img"]}>
+            <img src="images/we.png" alt="" loading="lazy" className={styles["aboutUs-img-img"]} />
+            <div className={styles["aboutUs_desc_tablet"]}>
+              <SectionTitle
+                title={trans("aboutUs_title")}
+                desc={trans("aboutUs_desc_under_title")}
+              />
+              <CustomList />
+            </div>
           </div>
-        </div>
-        <div className={styles["aboutUs-info"]}>
-          <div className={styles["aboutUs_desc_desktop"]}>
-            <SectionTitle
-              title={trans("aboutUs_title")}
-              desc={trans("aboutUs_desc_under_title")}
-            />
-            <CustomList />
-          </div>
-          <img src="images/we.png" alt="" loading="lazy" className={styles["aboutUs-img-mobile"]} />
-          <p>
-            {trans("aboutUs_desc_under")}
-          </p>
-          <div className={styles["aboutUs-buttons"]}>
-            <ButtonBlue title={trans("aboutUs_button")} />
+          <div className={styles["aboutUs-info"]}>
+            <div className={styles["aboutUs_desc_desktop"]}>
+              <SectionTitle
+                title={trans("aboutUs_title")}
+                desc={trans("aboutUs_desc_under_title")}
+              />
+              <CustomList />
+            </div>
+            <img src="images/we.png" alt="" loading="lazy" className={styles["aboutUs-img-mobile"]} />
+            <p>
+              {trans("aboutUs_desc_under")}
+            </p>
+            <div className={styles["aboutUs-buttons"]}>
+              <ButtonBlue title={trans("aboutUs_button")} click={onOpen} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- components/SaveLife/index.jsx
+++ components/SaveLife/index.jsx
@@ -4,36 +4,44 @@ import saveLife from "../../public/images/saveLife.png";
 import ButtonBlueBorder from "../ButtonBlueBorder";
 import ButtonBlue from "../ButtonBlue";
 import { useTrans } from "../../hooks/useTrans";
+import PaymentForm from "../PaymentForm";
+import { useOpenHandlers } from "../../hooks/useOpenHandlers";
 
 export default function SaveLife() {
+
+  const { open, onOpen, onClose } = useOpenHandlers(false);
+
   const { trans } = useTrans()
   return (
-    <div id={styles.saveLife}>
-      <div className={styles["saveLife-container"]}>
-        <div className={styles["block-save"]}>
-          <div className={styles.background}>
-            <img src="images/saveLife.png" alt="" />
-            <div></div>
-          </div>
-          <div className={styles["save-content"]}>
-            <div className={styles["mobile_flex"]}>
-              <h1>{trans("save_life_title_1")}</h1>
-              <h1>{trans("save_life_title_2")}</h1>
+    <>
+      <PaymentForm open={open} onClose={onClose} />
+      <div id={styles.saveLife}>
+        <div className={styles["saveLife-container"]}>
+          <div className={styles["block-save"]}>
+            <div className={styles.background}>
+              <img src="images/saveLife.png" alt="" />
+              <div></div>
             </div>
-            <h2>
-              {trans("save_life_desc")}
-            </h2>
-            <div className={styles["button-save"]}>
-              <ButtonBlueBorder title={trans("save_life_button_border")} />
-              <ButtonBlue title={trans("save_life_button")} />
+            <div className={styles["save-content"]}>
+              <div className={styles["mobile_flex"]}>
+                <h1>{trans("save_life_title_1")}</h1>
+                <h1>{trans("save_life_title_2")}</h1>
+              </div>
+              <h2>
+                {trans("save_life_desc")}
+              </h2>
+              <div className={styles["button-save"]}>
+                <ButtonBlueBorder title={trans("save_life_button_border")} />
+                <ButtonBlue title={trans("save_life_button")} click={onOpen} />
+              </div>
             </div>
           </div>
-        </div>
-        <div className={styles["button-save-mobile"]}>
-          <ButtonBlueBorder title={trans("save_life_button_border")} />
-          <ButtonBlue title={trans("save_life_button")} />
+          <div className={styles["button-save-mobile"]}>
+            <ButtonBlueBorder title={trans("save_life_button_border")} />
+            <ButtonBlue title={trans("save_life_button")} click={onOpen} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
