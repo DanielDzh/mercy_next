894ecc79fec5b7a3372b3c08ea4d9e202259102b
Mykola Melnyk <melnyk.cto.dev@gmail.com>
2022-10-14 00:44:31 +0300

    fix: update Review section

--- components/Reviews/index.jsx
+++ components/Reviews/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Slider from "react-slick";
 import styles from "./Reviews.module.scss";
 import { useTrans } from "../../hooks/useTrans";
@@ -6,72 +6,107 @@ import { SamplePrevArrow } from "../SlickSlider/SamplePrevArrow/SamplePrevArrow"
 import { SampleNextArrow } from "../SlickSlider/SampleNextArrow/SampleNextArrow";
 
 const sliderPaths = [
-   "images/reviews/review1.jpg",
-   "images/reviews/review2.jpg",
-   "images/reviews/review3.jpg",
-   "images/reviews/review4.jpg",
-   "images/reviews/review5.jpg",
+  {
+    id: 1,
+    desc: 'Дякую вам велике за гуманітарну допомогу,яку ви надали нам! Вона була вчасною для моєї сім\'ї.В мене 6 діток і ми дуже вдячні вам за продукти!',
+    author: 'Наталія, м.Суми'
+  },
+  {
+    id: 2,
+    desc: 'Ваша допомога стала великою підтримкою для мене і сина. Через постійні обстріли, ми вимушені були переселитися в Сумську область з Миколаєва. Але завдяки вашим зусиллям, наша адаптація стала легшою.',
+    author: 'Оксана, м.Ромни'
+  },
+  {
+    id: 4,
+    desc: 'Я приїхала до міста Запоріжжя з міста Щастя Луганської області, яке наразі окуповане. Дуже хотіла б повернутись додому. Дякую вам за допомогу!',
+    author: 'Вікторія, м. 3апоріжжя'
+  },
+  {
+    id: 5,
+    desc: 'Отримала допомогу. Дуже дякую! Зараз місто в нас обстрілюють. Дякую, що ви нас виручаєте!',
+    author: 'Переселенка з м. Гуляйполе'
+  },
+  {
+    id: 3,
+    desc: 'Після початку російської війни на території України, мій син втратив роботу, а в мене пенсія невелика. Через село, в якому я живу, пройшло кількасот російських танків, постраждали мирні люди. Район був окупований, і досі багато людей не можуть знайти собі роботу, бо її просто немає. Продукти, що ви дали, дуже допомогли мені і сину. Дякуємо вам!',
+    author: 'Ганна, м. Ромни'
+  },
+  {
+    id: 6,
+    desc: 'Хочу дуже подякувати фонду за те, що надали нашій родині гуманітарну допомогу у вигляді продуктового набору. Вона була для нашої сім\'ї вчасною.',
+    author: 'Наталія, м. Суми'
+  },
+  {
+    id: 7,
+    desc: 'Дякуємо за продуктові набори, це гарна допомога у ці скрутні часи та гарна підтримка для багатодітних сімей як ми!',
+    author: 'Сім\'я із м. Зеленодольськ'
+  },
+  {
+    id: 8,
+    desc: 'Ми дуже вдячні вам та благодійному фонду Mercy Chain за продуктову допомогу. Ми з Харкова, я інвалід-ліквідатор ЧАЕС, жінка пенсіонерка. Для нас це дуже велика допомога. Нехай Господь благословить вас та фонд за ваш труд та допомогу у потребах інших людей!',
+    author: 'Сім\'я з Харкова'
+  },
 ];
 export default function Reviews() {
 
-   const [activeSlide, setActiveSlide] = useState(0);
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 4,
+    speed: 300,
+    arrows: false,
+    dots: true,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 2,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1.5,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          infinite: true,
+        }
+      },
+    ],
+    prevArrow: <SamplePrevArrow />,
+    nextArrow: <SampleNextArrow />,
+  };
 
-   const settings = {
-      className: "center",
-      centerMode: true,
-      infinite: true,
-      centerPadding: "60px",
-      slidesToShow: 3,
-      speed: 300,
-      arrows: true,
-      dots: true,
-      cssEase: "linear",
-      responsive: [
-         {
-            breakpoint: 768,
-            settings: {
-               infinite: true,
-               arrows: false,
-            }
-         },
-         {
-            breakpoint: 600,
-            settings: {
-               slidesToShow: 1,
-               infinite: true,
-               arrows: false,
-            }
-         },
-      ],
-      beforeChange: next => setActiveSlide(next),
-      afterChange: current => setActiveSlide(current),
-      prevArrow: <SamplePrevArrow />,
-      nextArrow: <SampleNextArrow />,
-   };
+  const {trans} = useTrans();
 
-   const { trans } = useTrans();
-
-   return (
-      <div id="Reviews">
-         <div className={styles["reviews_container"]}>
-            <h1 className={styles["reviews_title"]}>
-               {trans("reviews_title")}
-            </h1>
-            <div className={styles["reviews_slider"]}>
-               <Slider {...settings}>
-                  {sliderPaths.map((p, index) => (
-                     <div className={styles["slider_wrapp"]} key={index + 1}>
-                        <img loading="lazy"
-                           className={`${styles['reviews_slide']} ${activeSlide === index ? styles["slide_center"] : styles["slide_not_center"]}`}
-                           // className={`${styles['reviews_slide']} ${activeSlide === index ? styles["slide_center"] : activeSlide === index + 1 ? styles["slide_after_slide"] : index === count ? styles["slide_before_slide"] : styles["slide_not_center"]}`}
-                           src={p}
-                           alt=""
-                        />
-                     </div>
-                  ))}
-               </Slider>
-            </div>
-         </div>
+  return (
+    <div id="Reviews">
+      <div className={styles["reviews_container"]}>
+        <h1 className={styles["reviews_title"]}>
+          {trans("reviews_title")}
+        </h1>
+        <div className={styles["reviews_slider"]}>
+          <Slider {...settings}>
+            {sliderPaths.map((review, index) => (
+              <div className={styles["slider_wrapp"]} key={index + 1}>
+                <div className='slider_content'>
+                  <p>{review.desc}</p>
+                  <h6>{review.author}</h6>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
       </div>
-   );
+    </div>
+  );
 }
